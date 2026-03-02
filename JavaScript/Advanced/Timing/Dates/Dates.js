42d2802d99b269a1177de7ce9c8aeff9bfb0c040
abunahim <abunahim5@gmail.com>
2026-03-02 08:50:06 +0000

    Add JavaScript Timing files

---
+++ JavaScript/Advanced/Timing/Dates/Dates.js
@@ -0,0 +1,47 @@
+//const date = new Date();
+//console.log(date);
+
+//Date(yr, mt, day, hr, min, sec, milSec)
+//const date = new Date(2026, 2, 5, 20, 10, 15, 15);
+//console.log(date);
+
+//const date = new Date("2026-02-05T20:10:15");
+//console.log(date);
+
+//const date = new Date(1700000000000000);
+//console.log(date);
+
+/*const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const dayOfWeek = date.getDay();
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minutes);
+console.log(seconds);
+console.log(dayOfWeek);*/
+
+/*const date = new Date();
+
+date.setFullYear(2026)
+date.setMonth(2);
+date.setDate(5);
+date.setHours(20);
+date.setMinutes(10);
+date.setSeconds(15);
+
+console.log(date);*/
+
+const date1 = new Date("2025-12-29");
+const date2 = new Date("2024-09-13");
+
+if(date2 < date1){
+    console.log("HAPPY NEW YEAR!");
+}
