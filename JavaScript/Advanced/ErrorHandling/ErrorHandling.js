9a78bec985a22b1b40d42a56bbde306ccb6fec61
abunahim <abunahim5@gmail.com>
2026-03-03 09:06:41 +0000

    Add JavaScript ErrorHandling files

---
+++ JavaScript/Advanced/ErrorHandling/ErrorHandling.js
@@ -0,0 +1,28 @@
+/*try{
+    console.log(x);
+}
+catch(error){
+    console.error(error);
+}
+finally{
+    console.log("This always executes");
+}
+console.log("You have reached the end!");*/
+
+try{
+    const dividend = window.prompt("Enter a dividend: ");
+    const divisor = window.prompt("Enter a divisor: ");
+
+    if(divisor == 0){
+        throw new Error("You can't divide by zero");
+    }
+    if(isNaN(dividend) || isNaN(divisor)){
+        throw new Error("values must be a number");
+    }
+    const result = dividend / divisor;
+    console.log(result);
+}
+catch(error){
+    console.error(error);
+}
+console.log("You have reached the end!");
